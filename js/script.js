4996037e9c0a50e2746623d6044ea7942186dc44
andrea-mosca <andrea.moscateli@gmail.com>
2025-07-29 15:25:18 +0200

    aggiunta errori

--- js/script.js
+++ js/script.js
@@ -5,14 +5,30 @@ async function fetchJson(url) {
 }
 
 async function getChefBirthday(id) {
-  const responseRicetta = await fetch(`https://dummyjson.com/recipes/${id}`);
-  const ricetta = await responseRicetta.json();
-  const responseUser = await fetch(
-    `https://dummyjson.com/users/${ricetta.userId}`
-  );
-  const user = await responseUser.json();
+  let ricetta;
+  try {
+    ricetta = await fetchJson(`https://dummyjson.com/recipes/${id}`);
+  } catch (err) {
+    throw new Error(`impossibile raggiungere la ricetta con id: ${id}`);
+  }
+  if (ricetta.message) {
+    throw new Error(ricetta.message);
+  }
+
+  let user;
+  try {
+    user = await fetchJson(`https://dummyjson.com/users/${ricetta.userId}`);
+  } catch (err) {
+    throw new Error(
+      `impossibile raggiungere lo chef con id: ${ricetta.userId}`
+    );
+  }
+  if (user.message) {
+    throw new Error(user.message);
+  }
   return user.birthDate;
 }
+
 try {
   getChefBirthday(1).then((res) =>
     console.log("Data di nascita dello chef:", res)
