99adeda511280b2f208c15c00390beee77025c43
andrea-mosca <andrea.moscateli@gmail.com>
2025-07-29 15:12:01 +0200

    creata funzione fetchJson

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+async function fetchJson(url) {
+  const response = await fetch(url);
+  const obj = await response.json();
+  return obj;
+}
+
 async function getChefBirthday(id) {
   const responseRicetta = await fetch(`https://dummyjson.com/recipes/${id}`);
   const ricetta = await responseRicetta.json();
@@ -7,7 +13,10 @@ async function getChefBirthday(id) {
   const user = await responseUser.json();
   return user.birthDate;
 }
-
-getChefBirthday(1)
-  .then((res) => console.log("Data di nascita dello chef:", res))
-  .catch((error) => console.error("Errore:", error.message));
+try {
+  getChefBirthday(1).then((res) =>
+    console.log("Data di nascita dello chef:", res)
+  );
+} catch (err) {
+  console.error(err);
+}
